3860593abe3f88534cef1bd2b833e70d136cd541
lcmongwe <lcmongwe@gmail.com>
2022-05-07 21:19:42 +0300

    installed flask

---
+++ config.py
@@ -0,0 +1,35 @@
+import os
+
+class Config:
+
+ 
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    # UPLOADED_PHOTOS_DEST ='app/static/photos'
+   
+
+
+    #  email configurations
+    # MAIL_SERVER = 'smtp.googlemail.com'
+    # MAIL_PORT = 587
+    # MAIL_USE_TLS = True
+    # MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    # MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
+class ProdConfig(Config):
+    pass
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://lucy:4444@localhost/watchlist_test'
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://lucy:4444@localhost/watchlist'
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
+
