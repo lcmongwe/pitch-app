f3062a9cad7d45f6b9a4a26f7b773801511968f7
lcmongwe <lcmongwe@gmail.com>
2022-05-10 22:17:32 +0300

    user model upgraded

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 # from email.policy import default
 # from enum import unique
+from unicodedata import category
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
@@ -11,7 +12,7 @@ class Note(db.Model):
     __tablename__ = 'note'
 
     id = db.Column(db.Integer,primary_key = True)
-    data = db.Column(db.String(1000))
+    category = db.Column(db.String(1000))
     data = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
@@ -27,7 +28,11 @@ class User(db.Model,UserMixin):
     email = db.Column(db.String(255), unique=True)
     pass_secure = db.Column(db.String(255))
     username = db.Column(db.String(255))
-    notes = db.relationship('Note',backref = 'note',lazy="dynamic")
+    notes = db.relationship('Note',backref = 'user',lazy="dynamic")
+
+    
+
+    
 
     @login_manager.user_loader
     def load_user(user_id):
