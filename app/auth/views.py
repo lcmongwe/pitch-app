5a20f0da7b013d044276e2219c132c5cdf32c7c4
lcmongwe <lcmongwe@gmail.com>
2022-05-11 00:37:33 +0300

    comments added

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,10 +2,39 @@ from flask import render_template,redirect,url_for,flash,request
 from . import auth
 from flask_login import login_user,logout_user,login_required
 from ..models import User
-from .forms import SignUpForm,LoginForm
+from .forms import LoginForm,RegistrationForm
 from .. import db
 from ..email import mail_message
 
+
+
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email = form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user,form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid username or Password')
+
+    title = "Pitch Login"
+    return render_template('auth/login.html',pitch_form =form,title=title)
+
+
+
+
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)
 
 
 @auth.route('/logout')
@@ -14,30 +43,38 @@ def logout():
     logout_user()
     return redirect(url_for("main.index"))
 
-@auth.route('/sign_up',methods = ["GET","POST"])
-def register():
-    form = SignUpForm()
-    if form.validate_on_submit():
-        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
-        db.session.add(user)
-        db.session.commit()
 
-        # mail_message("Welcome to pitches","email/welcome_user",user.email,user=user)
 
-        return redirect(url_for('auth.login'))
-    title = "New Account"
-    return render_template('auth/sign_up.html',registration_form = form, title=title)
+# @auth.route('/logout')
+# @login_required
+# def logout():
+#     logout_user()
+#     return redirect(url_for("main.index"))
 
-@auth.route('/login',methods=['GET','POST'])
-def login():
-    login_form = LoginForm()
-    if login_form.validate_on_submit():
-        user = User.query.filter_by(email = login_form.email.data).first()
-        if user is not None and user.verify_password(login_form.password.data):
-            login_user(user,login_form.remember.data)
-            return redirect(request.args.get('next') or url_for('main.index'))
+# @auth.route('/sign_up',methods = ["GET","POST"])
+# def register():
+#     form = SignUpForm()
+#     if form.validate_on_submit():
+#         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+#         db.session.add(user)
+#         db.session.commit()
 
-        flash('Invalid username or Password')
+#         # mail_message("Welcome to pitches","email/welcome_user",user.email,user=user)
 
-    title = "Pitch login"
-    return render_template('auth/login.html',login_form = login_form,title=title)
+#         return redirect(url_for('auth.login'))
+#     title = "New Account"
+#     return render_template('auth/sign_up.html',registration_form = form, title=title)
+
+# @auth.route('/login',methods=['GET','POST'])
+# def login():
+#     login_form = LoginForm()
+#     if login_form.validate_on_submit():
+#         user = User.query.filter_by(email = login_form.email.data).first()
+#         if user is not None and user.verify_password(login_form.password.data):
+#             login_user(user,login_form.remember.data)
+#             return redirect(request.args.get('next') or url_for('main.index'))
+
+#         flash('Invalid username or Password')
+
+#     title = "Pitch login"
+#     return render_template('auth/login.html',login_form = login_form,title=title)
--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from config import config_options
 from app import create_app,db
 from flask_script import Manager,Server
-from app.models import User,Note
+from app.models import *
 from  flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
@@ -16,7 +16,7 @@ manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db=db,User=User,Note=Note )
+    return dict(app = app,db=db,User=User,Pitch=Pitch,Comment=Comment)
 
 
 if __name__ == '__main__':
