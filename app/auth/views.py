14aa3d3b3a5ce3d494e371e821ac6f71839ea2c3
lcmongwe <lcmongwe@gmail.com>
2022-05-11 14:40:04 +0300

    requirements file created

--- app/auth/views.py
+++ app/auth/views.py
@@ -29,7 +29,7 @@ def login():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        user = User(email = form.email.data, username = form.username.data,pass_secure = form.password.data)
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('auth.login'))
