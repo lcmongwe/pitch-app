9019db064ecbc43479535dade12e969f3b6452cb
lcmongwe <lcmongwe@gmail.com>
2022-05-12 07:14:54 +0300

    profile route created

--- app/main/views.py
+++ app/main/views.py
@@ -125,6 +125,14 @@ def downvote(pitch_id):
     return redirect(url_for('main.index'))
 
 
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
 
 
 # Views
